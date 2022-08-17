b7fc2ae12906830667a8a9023afe99bc8b42b59a
Bryan Gutierrez <soybryanguti98@gmail.com>
2022-08-17 16:33:23 -0400

    Add .gitignore file and javascript directory

---
+++ web/javascript/index.js
@@ -0,0 +1,22 @@
+const number = document.getElementById('number');
+const searchOutButton = document.getElementById('searchOut');
+
+searchOutButton.addEventListener('click', searchOut);
+
+function searchOut(){
+    let intNumber = parseInt(number.value);
+
+    for (let i = 2; i <= intNumber; i++) {
+        if (parseInt(intNumber % i) > 0) {
+            continue;
+        }
+        if (parseInt(intNumber % i) === 0 && intNumber === i ) {
+            console.log("Es primo");
+            break;
+        }
+        if (parseInt(intNumber % i) === 0) {
+            console.log("No es primo");
+            break;
+        }
+    }
+}
